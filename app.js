7d9bd8a221bb044bfd65f4bb434b0bc15a6a9be6
filipeaaugusto <95827940+filipeaaugusto@users.noreply.github.com>
2021-12-08 21:54:23 -0300

    Ultima do dia

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({
-        mensagem:'Rota do tipo GET com endereço'
-    })
+app.use(express.json())
+
+// importar models
+const users = require('./models/users.model')
+
+mongoose.connect('mongodb://localhost:27017/Cookmaster', {
+    //opções]
+
+}).then(() => {
+    console.log('Conectado ao banco de dados com sucesso')
+}).catch((err) => {
+    console.log(err)
 })
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        mensagem:"Rota do tipo GET com endereço",
+        data:"08/12/2021",
+        author:"Filipe",
+        location: "Senai - Imperatriz/MA"
+    });
+});
+
+app.post('/novoUsuario', (req, res) => {
+    const dados = req.body
+    console.log (dados)
+    return res.status(201).json({
+       mensagem:'Usuário cadastrado com sucesso'
+    });
+});
 
 app.listen(5000, ()=>  {
     console.log('servidor rodando na porta 5000');
-})
+});
